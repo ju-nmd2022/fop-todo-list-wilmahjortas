7af22fb81935ed0ab9a7d5ee542a7b1480efe164
wilmahjortas <hjwi22oe@student.ju.se>
2023-04-12 10:03:36 +0200

    local commit

--- to-do-list.js
+++ to-do-list.js
@@ -1,9 +1,61 @@
+let taskArray = [];
+
+const listElement = document.getElementById("taskList");
+
 const addTaskButton = document.getElementById("addTaskButton");
 const container = document.getElementById("container");
 
-addTaskButton.addEventListener("click", addContent);
+if (localStorage.inputValue) {
+  taskArray = JSON.parse(localStorage.inputValue);
+}
+
+addTaskButton.addEventListener("click", function () {
+  taskLoop();
+  addContent();
+});
 
 function addContent() {
+  let inputField = document.getElementById("input");
+  const inputValue = inputField.value;
+
+  taskArray.push(inputValue);
+  localStorage.inputValue = JSON.stringify(taskArray);
+  console.log(taskArray);
+}
+
+function taskLoop() {
+  listElement.innerHTML = "";
+  for (let task of taskArray) {
+    const taskElement = document.createElement("div");
+    taskElement.innerText = task;
+    listElement.appendChild(taskElement);
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    taskElement.appendChild(removeButton);
+    removeButton.classList.add("remove");
+
+    const index = taskArray.indexOf(task);
+
+    removeButton.addEventListener("click", () => {
+      taskArray.splice(index, 1);
+      taskLoop();
+    });
+
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "Complete";
+    taskElement.appendChild(completeButton);
+    completeButton.classList.add("complete");
+
+    completeButton.addEventListener("click", () => {
+      taskElement.style.textDecoration = "line-through";
+    });
+  }
+}
+
+taskLoop();
+
+/* function addContent() {
   const newTaskElement = document.createElement("div");
 
   const newInputElement = document.createElement("input");
@@ -27,4 +79,4 @@ function addContent() {
   newTaskElement.appendChild(buttonElement);
 
   container.appendChild(newTaskElement);
-}
+} */
